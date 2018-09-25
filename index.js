fd05b85d4883e8fd4b6630ef8a2c867aafb9d9be
Cliff Jurgensen <jivefox.002@gmail.com>
2018-09-25 04:00:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your functions and code here
+var kittens = ['Milo', 'Otis', 'Garfield'];
+function destructivelyAppendKitten(name) {
+  kittens.push('Ralph');
+  return kittens
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift('Bob');
+  return kittens
+}
+function destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+function destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
+function appendKitten(name) {
+  return [...kittens, 'Broom']
+}
+function prependKitten(name) {
+  return ['Arnold', ...kittens]
+}
+function removeLastKitten() {
+  return kittens.slice(0,2)
+}
